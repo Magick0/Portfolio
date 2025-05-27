b9e47302303ccf9b44de879744166c25852796e3
Titouan <tlouisre56730@gmail.com>
2025-05-27 17:30:30 +0200

    plus que les trad a faire

--- script/langues.js
+++ script/langues.js
@@ -56,3 +56,22 @@ document.querySelectorAll("[data-i18n]").forEach((el) => {
     }
 });
 }
+
+
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Fermer si clic en dehors de la modale
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
