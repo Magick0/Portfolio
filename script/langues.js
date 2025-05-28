13751f3a3b841268dd49466c37481a93185449d1
Titouan <tlouisre56730@gmail.com>
2025-05-28 22:22:48 +0200

    menu burger

--- script/langues.js
+++ script/langues.js
@@ -75,3 +75,11 @@ window.onclick = function(event) {
     }
   });
 };
+
+
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("show");
+  
+}
+
